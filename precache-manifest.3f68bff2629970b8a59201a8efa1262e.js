ae443a0967ba0d8d1f633ab9a2b4c679d8169af0
Cedric B <cedricbardaine@gmail.com>
2019-10-24 17:52:28 +0200

    Updates

---
+++ precache-manifest.3f68bff2629970b8a59201a8efa1262e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1e65f960cf60801def51c009d1a684",
+    "url": "/TodoListReact/index.html"
+  },
+  {
+    "revision": "996abd71ec03731c69ac",
+    "url": "/TodoListReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c3f2901306276476317c",
+    "url": "/TodoListReact/static/js/2.c56718d9.chunk.js"
+  },
+  {
+    "revision": "996abd71ec03731c69ac",
+    "url": "/TodoListReact/static/js/main.538ff1d5.chunk.js"
+  },
+  {
+    "revision": "6177cad9b3057b42563f",
+    "url": "/TodoListReact/static/js/runtime-main.c8cf7c43.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TodoListReact/static/media/logo.25bf045c.svg"
+  }
+]);
